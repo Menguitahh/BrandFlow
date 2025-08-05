18f4e37b0c86343f4e571500bdfe9dcd4998d74c
Luciano Mengarelli <144611932+Menguitahh@users.noreply.github.com>
2025-08-05 16:08:10 -0300

    arreglos y pruebas automaticas

--- BrandFlow/settings.py
+++ BrandFlow/settings.py
@@ -100,6 +100,16 @@ DATABASES = {
     }
 }
 
+# Configuración para pruebas
+import sys
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

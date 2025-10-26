b49d78b4283821343aa52df24f5f708ef4be027c
Luciano Mengarelli <144611932+Menguitahh@users.noreply.github.com>
2025-10-26 16:02:09 -0300

    Update settings.py

--- BrandFlow/settings.py
+++ BrandFlow/settings.py
@@ -150,13 +150,13 @@ AUTH_USER_MODEL = 'user_control.Users'
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
     ],
 }
+
 
 # JWT Settings
 from datetime import timedelta

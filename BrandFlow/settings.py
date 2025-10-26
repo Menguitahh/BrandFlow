d83bb177692761407717aaaa734e1d9a35eb8730
Luciano Mengarelli <144611932+Menguitahh@users.noreply.github.com>
2025-10-26 13:23:04 -0300

    Update settings.py

--- BrandFlow/settings.py
+++ BrandFlow/settings.py
@@ -22,15 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY') or 'vdhg%DRS&56ASD&A&%Drfsavda!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 't')
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'brand_control',
